9ba42df3d3891722b4092456032fc718c86cc49d
4ewhatGIT <4ewhat@gmail.com>
2023-09-16 21:56:35 +1000

    Data transfer Update!
    In this update we added ability to transfer data through json files between our files!
    
    Follow for more updates!

--- core.py
+++ core.py
@@ -1,10 +1,25 @@
-from main import main_input
+import json
 
 # TODO: make an automatic module import
 # for module in modules:
 #     import module
+core_input = str
+
+def debug(inpt):
+    return inpt + ' it works'
 
 
-core_input = main_input
+def update():
+    with open("core_transfer.json", 'r') as f:
+        data = json.load(f)
+        core_input = data["main_input"]
 
-core_output = core_input + ' it works!'
+    core_output = debug(core_input)
+
+    with open("core_transfer.json", 'w+') as f:
+        print(data)
+        data['main_output'] = core_output
+        print(data)
+        json.dump(data, f)
+
+
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import json
+import core
 
 
-main_input = input('test input')
+data = []
+main_input = ''
+main_output = ''
 
-print(core_output)
+def main():
+    main_input = input('test: ')
+    with open("core_transfer.json", 'r') as f:
+        data = json.load(f)
+        print(data)
+
+    with open("core_transfer.json", 'w+') as f:
+        print(data)
+        data['main_input'] = main_input
+        print(data)
+        json.dump(data, f)
+
+    with open("core_transfer.json", 'r') as f:
+        core.update()
+        data = json.load(f)
+        main_output = data["main_output"]
+
+    print(main_output)
+
+if __name__ == '__main__':
+    main()
