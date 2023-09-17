fd16073f5fa5dd7eb1605811f5fe06c14a467360
NeykShon <126325430+NeykShon@users.noreply.github.com>
2023-09-17 20:49:14 +1000

    работа над приложениями

--- app_manager.py
+++ app_manager.py
@@ -1,14 +1,20 @@
 import json
 
+from system_progs import *
+
 
 def core():
     configs_ = open("configs.json", "r", encoding="utf-8")
     language_pack_ = open("system_languages.json", "r", encoding="utf-8")
+
     configs = json.load(configs_)
     language_pack = json.load(language_pack_)
+
+    configs_.close()
+    language_pack_.close()
+
+    del configs_, language_pack_
     language = language_pack[configs["current_language"]]
-    del configs_, language_pack_, language_pack
+    del language_pack
 
     print(f"{language['start']} {configs['product_name']}...")
-
-
--- system_progs/users/script.py
+++ system_progs/users/script.py
@@ -1,1 +1,14 @@
+import json
+import os.path
+import sys
 
+sys.path.append(os.path.join(os.getcwd(), "..\.."))
+print(sys.path)
+
+a = open(f"{sys.path[-1]}", "r")
+
+users_data_ = open("users_data.json", "r", encoding="utf-8")
+users_data = json.load(users_data_)
+del users_data_
+
+print(users_data["default_user"]["name"])
