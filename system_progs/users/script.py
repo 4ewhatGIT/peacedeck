16980b6e0eeef7ab91a311a4b4de803fddbdbf15
NeykShon <126325430+NeykShon@users.noreply.github.com>
2023-09-18 15:24:11 +1000

    работа над приложениями

--- system_progs/users/script.py
+++ system_progs/users/script.py
@@ -1,14 +1,25 @@
 import json
-import os.path
-import sys
 
-sys.path.append(os.path.join(os.getcwd(), "..\.."))
-print(sys.path)
-
-a = open(f"{sys.path[-1]}", "r")
+configs_ = open("../../configs.json", "r", encoding="utf-8")
+configs = json.load(configs_)
+del configs_
 
 users_data_ = open("users_data.json", "r", encoding="utf-8")
 users_data = json.load(users_data_)
 del users_data_
 
-print(users_data["default_user"]["name"])
+users_names_list = [[f"{users_data['default_user']['name'][configs['current_language']]}",
+                     f"{users_data['default_user']['id']}"]]
+
+if len(list(users_data)) >= 2:
+    users_names_list_ = list(users_data)[1:]
+    for i in range(len(users_names_list_)):
+        users_names_list.append([users_data[users_names_list_[i]]["name"],
+                                 users_data[users_names_list_[i]]["id"]])
+    del users_names_list_
+
+# print(users_names_list) debug
+
+print("")
+for i in range(len(users_names_list)):
+    print()
