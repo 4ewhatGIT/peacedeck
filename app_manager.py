a535d26db8786abdf0b957e6421f183ec8a9c40b
NeykShon <neikshon34@gmail.com>
2024-02-04 14:11:15 +1000

    Работа с приложениями

--- app_manager.py
+++ app_manager.py
@@ -3,40 +3,49 @@ import os
 import importlib.util
 
 
-def initialize():
+class Core:
     configs_ = open("configs.json", "r", encoding="utf-8")
-    language_pack_ = open("system_languages.json", "r", encoding="utf-8")
-
     configs = json.load(configs_)
-    language_pack = json.load(language_pack_)
-
     configs_.close()
-    language_pack_.close()
+    del configs_
 
-    del configs_, language_pack_
-    UI = language_pack[configs["current_language"]]
-    del language_pack
+    # Загрузка системы
+    def initialize(self):
+        language_pack_ = open("system_languages.json", "r", encoding="utf-8")
+        language_pack = json.load(language_pack_)
+        language_pack_.close()
 
-    print(f"{UI['start']} {configs['product_name']} {UI['version']} {configs['version']} ...")
+        del language_pack_
+        UI = language_pack[Core.configs["current_language"]]
+        del language_pack
 
+        print(f"{UI['start']} {Core.configs['product_name']} {UI['version']} {Core.configs['version']} ...")
 
-def get_packages(path):
-    packages = []
-    for item in os.listdir(path):
-        if os.path.isdir(os.path.join(path, item)):
-            packages.append(item)
-    return packages
+    # Получение данных о пакетах
+    def get_packages_data(self, path):
+        packages_ = []
+        for i in os.listdir(path):
+            if os.path.isdir(os.path.join(path, i)):
+                packages_.append(i)
 
+        packages = {}
+        for i in packages_:
+            app_info_ = open(f"{path}/{i}/app_info.json", "r", encoding="utf-8")
+            app_info = json.load(app_info_)
+            app_info_.close()
+            del app_info_
 
-def run_script(package_name, function_name):
-    package_path = os.path.join("system_progs", package_name)
-    spec = importlib.util.spec_from_file_location("script", os.path.join(package_path, "script.py"))
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    getattr(module, function_name)()
+            if Core.configs["current_language"] in app_info["title"]:
+                print(app_info["title"][f"{Core.configs['current_language']}"])
+            else:
+                print(app_info["title"]["default"])
 
+        return packages
 
-packages = get_packages("system_progs")
-print("Packages in system_progs: ", packages)
-obed = input("module: ")
-run_script(obed, "run")
+    # Запуск скрипта пакета
+    def run_script(self, package_name, function_name):
+        package_path = os.path.join("system_progs", package_name)
+        spec = importlib.util.spec_from_file_location("script", os.path.join(package_path, "script.py"))
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        getattr(module, function_name)()
--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from app_manager import *
 
 if __name__ == "__main__":
     print("start\n")
-    initialize()
+    Core = Core()
+    Core.initialize()
+    Core.get_packages_data("system_progs")
